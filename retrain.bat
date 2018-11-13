a12c9e6a65144026170cec00726a3c3c153fb541
freenowill <40315258+freenowill@users.noreply.github.com>
2018-11-13 14:39:00 +0800

    retrain.bat
    进行图像识别训练的批处理文件

---
+++ retrain.bat
@@ -0,0 +1,8 @@
+python C:/Users/zhuha/PycharmProjects/digital_recognize/tensorflow-1.7.0/tensorflow/examples/image_retraining/retrain.py ^
+--bottleneck_dir bottleneck ^
+--how_many_training_steps 200 ^
+--model_dir C:/Users/zhuha/PycharmProjects/digital_recognize/inception_model ^
+--output_graph output_graph.pb ^
+--output_labels output_labels.txt ^
+--image_dir data/train/
+pause
